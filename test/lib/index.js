2b90c5b840d977ae7b59cf9b612fe852a8dd611a
Alexey Yaroshevich <zxqfox@gmail.com>
2015-12-30 08:14:32 +0300

    feat: add initial f-ty and tests

---
+++ test/lib/index.js
@@ -0,0 +1,34 @@
+var concap = require('../../');
+
+describe('concap', function() {
+  it('should return and clean an array on calling getClean', function() {
+    concap.getClean().should.be.an('array').and.empty;
+  });
+
+  it('should capture calls with arguments into buffer', function() {
+    concap.hijack();
+    console.log('first argument', 'second argument');
+    console.warn('second call');
+    concap.restore();
+
+    var logs = concap.getClean();
+
+    logs.should.be.an('array').with.length(2);
+    logs[0].should.containSubset({method: 'log', args: ['first argument', 'second argument']});
+    logs[1].should.containSubset({method: 'warn', args: ['second call']});
+  });
+
+  it('should emulate time-timeEnd combos with .log', function(done) {
+    concap.hijack();
+    console.time('xxx');
+    setTimeout(() => {
+      console.timeEnd('xxx');
+      concap.restore();
+
+      var msg = concap.getClean()[0].args[0];
+      msg.should.match(/^xxx: [\d\.]+ms$/);
+      msg.replace(/^.*?([\d\.]+).*$/, '$1').should.be.above(3).and.be.below(10);
+      done();
+    }, 3);
+  });
+});
